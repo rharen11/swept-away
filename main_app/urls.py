076e8ca63e79db73e511e627b3acabdbb1c2675f
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-08 10:02:07 -0700

    index cleaned items

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('about/', views.about, name='about')
+  path('about/', views.about, name='about'),
+  path('items/', views.items_index, name='items_index')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,4 +8,17 @@ def home(request):
   return HttpResponse('<h1>Swept Away</h1>')
 
 def about(request):
-  return HttpResponse('<h1>Welcome to Swept Away! You will clean yourself happy</h1>')
+  return render(request, 'about.html')
+
+class Item:
+  def __init__(self, task, date):
+    self.task = task
+    self.date = date
+
+items = [
+  Item('bathroom', '3/3/22'),
+  Item('kitchen floor', '5/6/22')
+]
+
+def items_index(request):
+  return render(request, 'items/index.html', {'items': items})
