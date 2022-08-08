dd487b2448fc3904053ae2ffa61fd18381c5cd1f
Rachel Kinzel <rachel.l.kinzel@gmail.com>
2022-08-08 10:22:13 -0700

    home with sponge

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Swept Away</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
@@ -17,7 +17,8 @@ class Item:
 
 items = [
   Item('bathroom', '3/3/22'),
-  Item('kitchen floor', '5/6/22')
+  Item('kitchen floor', '5/6/22'),
+  Item('desk', '6/12/22')
 ]
 
 def items_index(request):
